94e0fbe6e25831f3460e1bfbb5d7bfa61f1ac0de
Silvia Rocha <szalbuque@gmail.com>
2023-03-04 15:49:01 -0300

    update terraform files

--- README.md
+++ README.md
@@ -128,4 +128,5 @@ Alterações realizadas:
 Ainda com erro. Usei o terraform destroy.
 Vou criar um main.tf que faça a configuração completa, incluindo rede e grupo de segurança.
 * Isso pode ter algo a ver com as credenciais de acesso do IAM user? Me parece que não, porque isso causaria o não funcionamento da criação das instâncias. As instâncias foram criadas, mas não consegui acessar por SSH. Pode ser algum erro na configuração do grupo de segurança, ou faltou configurar corretamente o firewall.
+* Para criar a VPC usando o terraform, preciso atribuir mais política de acesso <b>AmazonVPCFullAccess</b> ao usuário terraform-user, que criei para operar com o terraform.
 * 
--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,59 @@ provider "aws" {
   region = "us-west-2"
 }
 
+/*
+Configures a Virtual Private Cloud (VPC) module, which provisions networking resources
+such as a VPC, subnets, and internet and NAT gateways based on the arguments provided.
+Uses the definitions in the file "variables.tf".
+*/
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.18.1"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
+}
+
+# defines a key pair that will be assigned to the EC2 instances
+
+module "key-pair" {
+  source  = "terraform-aws-modules/key-pair/aws"
+  version = "2.0.2"
+
+  name = var.key_name
+  
+}
+
+#  defines three EC2 instances provisioned within the VPC created by the module.
+
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "4.3.0"
+
+  count = 3
+  name  = "dio-app-ec2-cluster-${count.index}"
+
+  ami                    = "ami-0c5204531f799e0c6"
+  instance_type          = "t2.micro"
+  key_name                = "user1"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+------------------
 resource "aws_instance" "app" {
   count                  = 3
   ami                    = "ami-830c94e3"
@@ -27,14 +80,14 @@ resource "aws_instance" "app" {
   }
 }
 
-resource "aws_key_pair" "TF_key" {
-  key_name = "TF_key"
-  public_key = tls_private_key.rsa.public_key_openssh
-}
-
 resource "tls_private_key" "rsa" {
   algorithm = "RSA"
   rsa_bits = 4096
+}
+
+resource "aws_key_pair" "TF_key" {
+  key_name = "TF_key"
+  public_key = tls_private_key.rsa.public_key_openssh
 }
 
 resource "local_file" "TF_key" {
